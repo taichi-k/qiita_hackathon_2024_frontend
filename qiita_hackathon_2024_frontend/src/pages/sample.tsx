2d5758c390f636d279686ac577884c39ddd58740
TaichiK <tai@taimac.local>
2024-09-16 17:26:46 +0900

    next側で不要なファイルを修正

--- qiita_hackathon_2024_frontend/src/pages/sample.tsx
+++ qiita_hackathon_2024_frontend/src/pages/sample.tsx
@@ -12,7 +12,7 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-export default function Home() {
+export default function Sample() {
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
