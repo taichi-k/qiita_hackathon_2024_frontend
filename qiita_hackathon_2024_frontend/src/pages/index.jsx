0ea12fe163746bb028367f870d3df1e0edd8669c
TaichiK <tai@taimac.local>
2024-09-17 21:08:42 +0900

    shadcnを導入

--- qiita_hackathon_2024_frontend/src/pages/index.jsx
+++ qiita_hackathon_2024_frontend/src/pages/index.jsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button"
+import { Textarea } from "@/components/ui/textarea"
+
+
 export default function Index() {
   return (
     <div>
       ここから
+      <Button variant="outline">Click me</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Textarea placeholder="Type your message here." className="my_width" />
     </div>
   );
 }
