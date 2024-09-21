984adc18513491660f5381243a8691a6a80ee53c
TaichiK <tai@taimac.local>
2024-09-21 13:15:43 +0900

    型周りを修正

--- qiita_hackathon_2024_frontend/src/components/Call.tsx
+++ qiita_hackathon_2024_frontend/src/components/Call.tsx
@@ -15,14 +15,14 @@ import AgoraRTC, {
   useRemoteUsers,
 } from "agora-rtc-react";
 
-function Call(props) {
+function Call(props: { channelName: string; appId: string }) {
   const client = useRTCClient(
     AgoraRTC.createClient({ codec: "vp8", mode: "rtc" })
   );
 
   return (
     <AgoraRTCProvider client={client}>
-      <Videos channelName={props.channelName} AppID={props.appId} />
+      <Videos channelName={props.channelName!} AppID={props.appId!} />
       <div className="fixed z-10 bottom-0 left-0 right-0 flex justify-center pb-4">
         <a
           className="px-5 py-3 text-base font-medium text-center text-white bg-red-400 rounded-lg hover:bg-red-500 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 w-40"
--- qiita_hackathon_2024_frontend/src/pages/channel/[channelName]/index.jsx
+++ qiita_hackathon_2024_frontend/src/pages/channel/[channelName]/index.jsx
@@ -5,15 +5,15 @@ import { useRouter } from 'next/router'
 
 const Call = dynamic(() => import("@/components/Call"), { ssr: false });
 
-export default function Page({ params }: { params: { channelName: string } }) {
+export default function Page( params ) {
     const router = useRouter()
     
     return (
         <main className="flex w-full flex-col">
             <p className="absolute z-10 mt-2 ml-12 text-2xl font-bold text-gray-900">
-                {router.query.channelName!}
+                {router.query.channelName}
             </p>
-            <Call appId={process.env.NEXT_PUBLIC_AGORA_APP_ID!} channelName={router.query.channelName}></Call>
+            <Call appId={process.env.NEXT_PUBLIC_AGORA_APP_ID} channelName={router.query.channelName}></Call>
         </main>
     )
 }
