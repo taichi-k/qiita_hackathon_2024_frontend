57d42afcd4f2c319eecc9f469e292f370665e6b2
ninoko1995 <ninoninomiyamiya1995@gmail.com>
2024-09-21 20:08:06 +0900

    fix console errors

--- qiita_hackathon_2024_frontend/src/components/tailblocks/Header.jsx
+++ qiita_hackathon_2024_frontend/src/components/tailblocks/Header.jsx
@@ -4,18 +4,18 @@ import Logo from "@/components/Logo"
 
 export default function Header() {
     return (
-        <header class="text-gray-600 body-font">
-            <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <a href="/lobby" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <header className="text-gray-600 body-font">
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <a href="/lobby" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                 <Logo></Logo>
-                <span class="ml-3 text-xl" id="generated-id-1726902635829-97n7j7e2p">Open Lobby</span>
+                <span className="ml-3 text-xl" id="generated-id-1726902635829-97n7j7e2p">Open Lobby</span>
                 </a>
-                <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                <a href="/lobby" class="mr-5 hover:text-gray-900" id="generated-id-1726902635829-adsfs">ロビー</a>
-                <a href="/settings" class="mr-5 hover:text-gray-900" id="generated-id-1726902635829-o0pzxlbba">設定</a>
-                <a href="/" class="mr-5 hover:text-gray-900" id="generated-id-1726902635829-9reuu9cas">ログアウト</a>
+                <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                <a href="/lobby" className="mr-5 hover:text-gray-900" id="generated-id-1726902635829-adsfs">ロビー</a>
+                <a href="/settings" className="mr-5 hover:text-gray-900" id="generated-id-1726902635829-o0pzxlbba">設定</a>
+                <a href="/" className="mr-5 hover:text-gray-900" id="generated-id-1726902635829-9reuu9cas">ログアウト</a>
                 </nav>
-                {/* <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0" id="generated-id-1726902635829-emxkyawu6">ログアウト</button> */}
+                {/* <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0" id="generated-id-1726902635829-emxkyawu6">ログアウト</button> */}
             </div>
         </header>
     )
