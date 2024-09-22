d50e27357a597f8ce3d1a36c8f2a25c60e74fc28
ninoko1995 <ninoninomiyamiya1995@gmail.com>
2024-09-22 11:17:52 +0900

    update toppage design

--- qiita_hackathon_2024_frontend/src/pages/index.jsx
+++ qiita_hackathon_2024_frontend/src/pages/index.jsx
@@ -4,20 +4,21 @@ import ServiceName from "@/components/ServiceName"
 
 export default function Index() {
   return (
-    <div className="w-full h-screen welcomeBox bg-gradient-to-br from-neutral-50 to-amber-50">
+    <div className="w-full h-screen welcomeBox bg-gradient-to-br from-neutral-50 to-amber-50 libraryBackground">
       <div className="mx-auto w-fit">
-        <div className="mb-4">
+        <div className="mb-4" style={{color:'white'}}>
           <ServiceName></ServiceName> で勉強を始めよう！
         </div>
         <div className="flex gap-8 items-center justify-center">
           <Link href="/login">
-            <Button className="border border-yellow-900" variant="outline">ログイン</Button>
+            <Button className="border border-white-900" variant="outline" style={{color:'white'}}>ログイン</Button>
           </Link>
           <Link href="/register">
             <Button variant="secondary">新規登録</Button>
           </Link>
         </div>
       </div>
+      <div className='top-footer'></div>
     </div>
   );
 }
